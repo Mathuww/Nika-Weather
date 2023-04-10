b278c4933bcf97a25ce62b85e6908a6cc83c3188
EuroCreator <matheo@darnaudguilhem.com>
2023-04-10 21:09:20 +0200

    Nettoyage propre
    et nette

--- NikaWeather/sql/valeurTest-NW_stats.sql
+++ NikaWeather/sql/valeurTest-NW_stats.sql
@@ -4,21 +4,25 @@ DELETE FROM nw_stats;
 
 INSERT INTO
     NW_stats (
+        id,
         date,
         time_zone,
         recep_temp_average
     )
 VALUES (
+        1,
         '2023-04-08 00:00:00',
         '+01:00',
         16
         /*(minimum : 11°, maximum : 22°)*/
     ), (
+        2,
         '2023-04-09 00:00:00',
         '+01:00',
         12
         /*(minimum : 7°, maximum : 17°)*/
     ), (
+        3,
         '2023-04-14 00:00:00',
         '+01:00',
         15
