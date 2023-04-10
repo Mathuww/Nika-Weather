82c902671fb1d178180d85934e1f05697f86286b
Mathuww <52327390+EuroCreator@users.noreply.github.com>
2023-04-10 15:51:56 +0200

    Algorithme de transfer de données
    De la table NW_receps à la table NW_stats si les dates diffèrent

--- NikaWeather/php/transfer_receps_to_stats.php
+++ NikaWeather/php/transfer_receps_to_stats.php
@@ -21,15 +21,42 @@ echo "<br>";
 echo "___________________________________________________________________________________________" . "<br>";
 echo "<br>";
 
-/*Requête SQL pour insérer des données dans la table NW_stats*/
-$sql_statsINSERT = 'INSERT INTO nw_stats(date, recep_temp_average) VALUES (:date, :recep_temp_average);';
-$insertStats = $mysqlClient->prepare($sql_statsINSERT);
 
-/*Avec les données de la table NW_recep de la première indentation*/
-$insertStats->execute([
-    'date' => $receps[0]["date"],
-    'recep_temp_average' => $receps[0]["recep_temp_average"]
-]);
+//Récupérer les données de la table stats
+$sql_stats = 'SELECT * FROM nw_stats';
+$statsStatement = $mysqlClient->prepare($sql_stats);
+$statsStatement->execute();
+$stats = $statsStatement->fetchAll();
+
+//Afficher les données de la table stats
+foreach ($stats as $stat) {
+?>
+    <p><?php
+        echo "C'était le " . $stat["date"] . " avec une température s'approchant des " . $stat["recep_temp_average"] . "°C." ?></p>
+<?php
+}
+
+
+echo "<br>";
+echo "___________________________________________________________________________________________" . "<br>";
+echo "<br>";
+
+
+//Ajoute les données de la table NW_recep à NW_stats si et seulement si ils ont des dates différentes
+foreach ($receps as $recep) {
+    $date = array_column($stats, 'date');
+    if (!array_search($recep['date'], $date)) {
+        /*Requête SQL pour insérer des données dans la table NW_stats*/
+        $sql_statsINSERT = 'INSERT INTO nw_stats(date, recep_temp_average) VALUES (:date, :recep_temp_average);';
+        $insertStats = $mysqlClient->prepare($sql_statsINSERT);
+
+        /*Avec les données de la table NW_recep de l'indexation de la variable $indexRechercheReceps*/
+        $insertStats->execute([
+            'date' => $recep["date"],
+            'recep_temp_average' => $recep["recep_temp_average"]
+        ]);
+    }
+}
 echo "Le transfert de données a bien fonctionné (si aucun message d'erreur est affiché).";
 
 
