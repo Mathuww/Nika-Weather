6bdb9e663a3ee3443d75cc3b48464b86666e87c4
Mathuww <matheo@darnaudguilhem.com>
2023-05-28 19:48:11 +0200

    Réparation des erreurs de mises en page

--- php/connect.php
+++ php/connect.php
@@ -4,14 +4,13 @@ $admins = getAdmin($dbNW);
 $adminsID = array_column($admins, 'username');
 $adminsPassword = array_column($admins, 'password');
 
-if(isset($_POST["input-adminID"]) and isset($_POST["input-adminPassword"]))
-{
+if (isset($_POST["input-adminID"]) and isset($_POST["input-adminPassword"])) {
     if (!in_array($_POST["input-adminID"], $adminsID)) {
         $signalClient = "L'identifiant fourni ne permet pas d'accéder aux paramètres administrateurs.";
     } elseif (!in_array($_POST["input-adminPassword"], $adminsPassword)) {
         $signalClient = "Votre mot de passe (faux!) ne correspond pas à nos données.";
     } else {
-        $signalClient = "Bienvenue " . $_POST["input-adminID"] . "chez Nika Weather";
+        $signalClient = "Bienvenue " . $_POST["input-adminID"] . " chez Nika Weather";
         // sleep(2.2);
         // header("Location: ./settings.php");
         //Ajouter une session pour être connecter
@@ -36,7 +35,6 @@ if(isset($_POST["input-adminID"]) and isset($_POST["input-adminPassword"]))
             <div class="NW-header__titles">
                 <h1 class="NW-header__titles--Primary zeroMargin title">Se connecter<br />en administrateurs</h1>
             </div>
-            <!-- <div class="NW-header__icons"> -->
             <?php if ($_SESSION["DL-USER"] == "light") : ?>
                 <a href="index.php" class="NW-header__logo">
                     <img src="../resources/icons/logo(LIGHT).png" alt="Icon Logo" />
@@ -46,23 +44,22 @@ if(isset($_POST["input-adminID"]) and isset($_POST["input-adminPassword"]))
                     <img src="../resources/icons/logo(DARK).png" alt="Icon Logo" />
                 </a>
             <?php endif; ?>
-            <!-- </div> -->
         </section>
         <section class="connect-Space">
-            <p class="connect-Space_advert subtitle"><?php 
-                echo $signalClient; 
-            ?></p>
+            <p class="connect-Space_advert subtitle"><?php
+                                                        echo $signalClient;
+                                                        ?></p>
             <form method="post" action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES); ?>" id="connect-Space_form">
                 <div class="connect-Space_form-id box-connect">
                     <label>IDENTIFIANT</label><br />
-                    <input for="connect-Space_form" name="input-adminID" type="text" autofocus required maxlength=32 minlength=4 tabindex=1 placeholder="ici, votre pseudo" autocomplete="nickname" />
+                    <input for="connect-Space_form" name="input-adminID" type="text" autofocus required maxlength=32 minlength=4 tabindex=1 placeholder="ici, votre pseudo" autocomplete="username" />
                 </div>
                 <div class="connect-Space_form-password box-connect">
                     <label>MOT DE PASSE</label><br />
                     <input for="connect-Space_form" name="input-adminPassword" type="password" required minlength=8 maxlength=72 tabindex=2 autocomplete="current-password" />
                 </div>
                 <div class="connect-Space_form-submit box-connect">
-                <button type="submit" class="btn"> Se connecter </button>
+                    <button type="submit" class="btn"> Se connecter </button>
                 </div>
             </form>
         </section>
